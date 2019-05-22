19d6b67e016ed509e0a489560ccaeec5b9f0d4b6
Nilay Patel <48822794+nilaypatel18@users.noreply.github.com>
2019-05-22 10:29:39 +0530

    Changed in files
    Changed in files

---
+++ .vuepress/theme/Home.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="home">
+		<div class="hero">
+			<img
+			v-if="data.heroImage"
+			:src="$withBase(data.heroImage)"
+			alt="hero"
+			>
+			<h1>{{ data.heroText || $title || 'Hello' }}</h1>
+			<p class="description">
+				{{ data.tagline || $description || 'Welcome to your VuePress site' }}
+			</p>
+
+			<p
+			class="action"
+			v-if="data.actionText && data.actionLink"
+			>
+
+		</p>
+		<div
+		class="features"
+		v-if="data.features && data.features.length"
+		>
+		<div
+		class="feature"
+		v-for="(feature, index) in data.features"
+		:key="index"
+		>
+		<h2>{{ feature.title }}</h2>
+		<p>{{ feature.details }}</p>
+	</div>
+</div>
+<Content custom/>
+
+</div>
+</div>
+</div>
+</template>
+<script>
+export default {
+	name : "Home",
+	computed:{
+		data(){
+			return this.$page.frontmatter
+		},
+		actionLink () {
+			return {
+				link: this.data.actionLink,
+				text: this.data.actionText
+			}
+		}
+	}
+}
+</script>
---
+++ .vuepress/theme/Layout.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="theme-container">
+    <Header></Header>
+    <Home></Home>
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home";
+export default {
+  name: "Layout",
+  components: { Header, Footer, Home },
+  data() {
+    return {
+      isSidebarOpen: false,
+      swUpdateEvent: null
+    };
+  },
+  computed: {
+    shouldShowNavbar() {
+      const { themeConfig } = this.$site;
+      const { frontmatter } = this.$page;
+      if (frontmatter.navbar === false || themeConfig.navbar === false) {
+        return false;
+      }
+      return (
+        this.$title ||
+        themeConfig.logo ||
+        themeConfig.repo ||
+        themeConfig.nav ||
+        this.$themeLocaleConfig.nav
+      );
+    },
+
+    shouldShowSidebar() {
+      const { frontmatter } = this.$page;
+      return (
+        !frontmatter.layout &&
+        !frontmatter.home &&
+        frontmatter.sidebar !== false &&
+        this.sidebarItems.length
+      );
+    },
+
+    sidebarItems() {
+      return resolveSidebarItems(
+        this.$page,
+        this.$route,
+        this.$site,
+        this.$localePath
+      );
+    },
+
+    pageClasses() {
+      const userPageClass = this.$page.frontmatter.pageClass;
+      return [
+        {
+          "no-navbar": !this.shouldShowNavbar,
+          "sidebar-open": this.isSidebarOpen,
+          "no-sidebar": !this.shouldShowSidebar
+        },
+        userPageClass
+      ];
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+
+    // configure progress bar
+    nprogress.configure({ showSpinner: false });
+
+    this.$router.beforeEach((to, from, next) => {
+      if (to.path !== from.path && !Vue.component(to.name)) {
+        nprogress.start();
+      }
+      next();
+    });
+
+    this.$router.afterEach(() => {
+      nprogress.done();
+      this.isSidebarOpen = false;
+    });
+
+    this.$on("sw-updated", this.onSWUpdated);
+  },
+  methods: {
+    toggleSidebar(to) {
+      this.isSidebarOpen = typeof to === "boolean" ? to : !this.isSidebarOpen;
+    },
+    // side swipe
+    onTouchStart(e) {
+      this.touchStart = {
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY
+      };
+    },
+    onTouchEnd(e) {
+      const dx = e.changedTouches[0].clientX - this.touchStart.x;
+      const dy = e.changedTouches[0].clientY - this.touchStart.y;
+      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
+        if (dx > 0 && this.touchStart.x <= 80) {
+          this.toggleSidebar(true);
+        } else {
+          this.toggleSidebar(false);
+        }
+      }
+    },
+    onSWUpdated(e) {
+      this.swUpdateEvent = e;
+    }
+  }
+};
+</script>
